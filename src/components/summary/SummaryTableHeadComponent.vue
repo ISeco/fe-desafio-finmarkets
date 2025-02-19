b6aa8da4ad480c6ce933c0e66f7342bf65c6914e
Ignacio Seco <ignacioseco006@gmail.com>
2025-02-19 09:10:29 -0300

    Refactor SummaryTableHeadComponent para mejorar la gestión de propiedades y aplicar estilos a través de una propiedad computada

--- src/components/summary/SummaryTableHeadComponent.vue
+++ src/components/summary/SummaryTableHeadComponent.vue
@@ -1,22 +1,33 @@
 <template>
   <th
-    :style="{verticalAlign: 'inherit', textAlign: alignment, textTransform: 'capitalize', fontSize: '10.5px', color: 'var(--font-color)', padding: '5px 0px'}">
-    {{ headvalue }}</th>
+    :style="thStyle">
+    {{ title }}
+  </th>
 </template>
+
 <script>
 export default {
   name: 'SummaryTableHeadComponent',
   props: {
-    title: String,
+    title: {
+      type: String,
+      required: true
+    },
     textAlignment: {
       type: String,
       default: 'start'
     }
   },
-  setup(props) {
-    return {
-      headvalue: props.title,
-      alignment: props.textAlignment
+  computed: {
+    thStyle() {
+      return {
+        verticalAlign: 'inherit',
+        textAlign: this.textAlignment,
+        textTransform: 'capitalize',
+        fontSize: '10.5px',
+        color: 'var(--font-color)',
+        padding: '5px 0px'
+      };
     }
   }
 }
